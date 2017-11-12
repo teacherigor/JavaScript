a4c37f196d17bda1f34bff7e8d4b2327a100a657
teacherigor <teacherigor6@gmail.com>
2017-11-12 14:33:51 +0200

    else if operator

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 var year = prompt("Enter the date when the ECMA-262 5.1 standard was released", "");
 
-if (year == 2011) {
-	alert ("You're JS-savvy!");
+if (year < 2011) {
+	alert ("It's too early...");
+} else if (year > 2011) {
+	alert ("It's a little bit late...");
 } else {
-	alert ("Way wrong answer!");
+	alert ("Yes, this year precisely!");
 }
