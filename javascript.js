bca74e93480440f80c72574c86c4b2953e42839e
Igor Semenuk <teacherigor6@gmail.com>
2017-11-15 16:07:47 +0200

    userName

--- javascript.js
+++ javascript.js
@@ -1,9 +1,19 @@
-var value = prompt('Enter your login', '');
+var userName = prompt("Who's there?", '');
 
-if (value == 'Admin') {
-	alert('Enter password');
-} else if (value == 'Other') {
-	alert("I don't know you!" );
-}else if (value == 'Escape') {
-	alert('Enter cancelled');
+if (userName == 'Admin') {
+
+	var pass = prompt('Enter your password', '');
+	if (pass == 'Black Lord') {
+		alert('Welcome!');
+	} else if (pass == null) {
+		alert('Way wrong answer!');
+	} else {
+		alert('Enter cancelled');
+	}
+
+
+} else if (userName == null) { // (**)
+	alert ('Enter cancelled');
+} else {
+	alert ("I don't know you!");
 }
