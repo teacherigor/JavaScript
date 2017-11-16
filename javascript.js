3c018ed5d38c8261227abd9f1560e09db0903492
Igor Semenuk <teacherigor6@gmail.com>
2017-11-16 17:00:18 +0200

    replace 'if' with '?'

--- javascript.js
+++ javascript.js
@@ -1,19 +1,8 @@
-var userName = prompt("Who's there?", '');
-
-if (userName == 'Admin') {
-
-	var pass = prompt('Enter your password', '');
-	if (pass == 'Black Lord') {
-		alert('Welcome!');
-	} else if (pass == null) {
-		alert('Way wrong answer!');
-	} else {
-		alert('Enter cancelled');
-	}
-
-
-} else if (userName == null) { // (**)
-	alert ('Enter cancelled');
+/*
+if (a + b < 4) {
+	result = 'Not enough';
 } else {
-	alert ("I don't know you!");
+	result = 'More than enough';
 }
+*/
+
