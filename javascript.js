48ac7ccb79c63712aec296d921cfe381df4250eb
teacherigor <teacherigor6@gmail.com>
2017-11-18 13:35:54 +0200

    if (1 || 0)

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-alert(true || true); // true
-alert(false || true); // true
-alert(true || false); // true
-alert(false || false); // false
+if (1 || 0) { // it will work as if (true || false)
+	alert('true');
+}
