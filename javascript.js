3e0d58274637c0f32d76e2de11888b774a19a125
teacherigor <teacherigor6@gmail.com>
2017-11-25 16:06:32 +0200

    sequence of operands with the && operator

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-// The first argument is true, that is why the second argument is returned
-alert(1 && 0); //0
-alert(1 && 5); //5
+alert(1 && 2 && null && 3); // null
 
-// The first argument is false, so it is returned, and the second argument is ignored
-alert(null && 5); // null, because it is false
-alert(0 && "No matter"); //0, because it is false
+alert(1 && 2 && 3); // 3
