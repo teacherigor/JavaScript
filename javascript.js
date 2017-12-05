ad47a5a0d793f6cb1b56890a22afe9daf0665d22
Igor Semenuk <teacherigor6@gmail.com>
2017-12-05 12:08:21 +0200

    Use bracket noation to retrieve the NthtoLast character

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-// Use bracket notation to get the character at other positions withing a string
+// Use bracket notation to the NthtoLast character
 var lastName = "Semenuk";
-var lastLetterOfLastName = lastName[lastName.length - 1];
+var lastLetterOfLastName = lastName[lastName.length - 4];
 
 alert(lastLetterOfLastName);
