04cbe2c9cd9139862a97b79df8dab5d89ac35afd
Igor Semenuk <teacherigor6@gmail.com>
2017-12-05 10:58:24 +0200

    Use bracket notation to find the last character

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // Use bracket notation to get the character at other positions withing a string
 var lastName = "Semenuk";
-var thirdLetterOfLastName = lastName[2];
+var lastLetterOfLastName = lastName[lastName.length - 1];
 
-alert(thirdLetterOfLastName);
+alert(lastLetterOfLastName);
