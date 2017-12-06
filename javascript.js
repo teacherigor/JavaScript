8988c228da11615942e113df06ed46aadcc5e8e5
Igor Semenuk <teacherigor6@gmail.com>
2017-12-06 12:00:20 +0200

    var sentence = wordBlanks

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
+// I declared a new var to print out the sentence
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 	var result = 'This ' + myAdjective + ' ' + myNoun + ' ' + myVerb + ' very ' + myAdverb + '.';
 	
 	return result;
 }
 
-wordBlanks("dog", "big", "ran", "quickly");
+var sentence = wordBlanks("dog", "big", "ran", "quickly");
+
+alert(sentence);
