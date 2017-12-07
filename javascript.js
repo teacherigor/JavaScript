df6100f1d6be9895941c5483009db8eb40ada99b
Igor Semenuk <teacherigor6@gmail.com>
2017-12-07 17:03:05 +0200

    ourArray

--- javascript.js
+++ javascript.js
@@ -1,5 +1,2 @@
-var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
-
-var myData = myArray[2][1];
-
-alert(myData);
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push("happy", "joy");
