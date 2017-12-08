a11cce41a7c4571418bf5ec74360c4057b66304e
Igor Semenuk <teacherigor6@gmail.com>
2017-12-08 15:00:09 +0200

    var myList

--- javascript.js
+++ javascript.js
@@ -1,7 +1,1 @@
-var ourArray = ["Stimpson", "J", ["cat"]];
-ourArray.shift();
-ourArray.unshift("Happy");
-
-var myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-myArray.unshift(["Paul", 35]);
+var myList = [["Chicken", 150], ["Razor", 14], ["Sprite", 22], ["Deodorant", 50], ["Tomatoes", 40]];
