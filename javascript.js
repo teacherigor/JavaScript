27a9f6b122842c7bcdf780b5ec17a147aa990494
Igor Semenuk <teacherigor6@gmail.com>
2017-12-08 11:57:07 +0200

    Array.unshift()

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 var ourArray = ["Stimpson", "J", ["cat"]];
-var removeFromOurArray = ourArray.shift();
+ourArray.shift();
+ourArray.unshift("Happy");
 
 var myArray = [["John", 23], ["dog", 3]];
-var removeFromMyArray = myArray.shift(["dog", 3]);
+myArray.shift();
+myArray.unshift(["Paul", 35]);
