f6fcb105283877ebfe8c6e4c61da99ac4931e57d
Igor Semenuk <teacherigor6@gmail.com>
2017-12-08 10:48:29 +0200

    Array.shift()

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-var ourArray = [1,2,3];
-var removeFromOurArray = ourArray.pop();
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removeFromOurArray = ourArray.shift();
 
-var myArray = [["John", 23], ["cat", 2]];
-var removeFromMyArray = myArray.pop(["cat", 2]);
+var myArray = [["John", 23], ["dog", 3]];
+var removeFromMyArray = myArray.shift(["dog", 3]);
