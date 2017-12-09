7d2ef569d69a1ef6289bb9e7e3f2d4971b0805b0
teacherigor <teacherigor6@gmail.com>
2017-12-09 17:14:31 +0200

    myLocalScope

--- javascript.js
+++ javascript.js
@@ -1,16 +1,7 @@
-var myGlobal = 10;
+function myLocalScope() {
+	'use strict';
+	var myVar;
 
-function fun1() {
-	oopsGlobal = 5;
+	console.log(myVar);
 }
-
-function fun2() {
-	var output = "";
-	if (typeof myGlobal != "undefined") {
-		output += "myGlobal:" + myGlobal;
-	}
-	if (typeof oopsGlobal != "undefined") {
-		output += " oopsGlobal: " + oopsGlobal;
-	}
-	console.log(output);
-}
+myLocalScope();
