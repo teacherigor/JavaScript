7e43c6d743c5f73a942d59000d50815985ac8e89
Igor Semenuk <teacherigor6@gmail.com>
2017-12-19 12:07:03 +0200

    switch vs else if

--- javascript.js
+++ javascript.js
@@ -1,25 +1,25 @@
 'use strict';
-function sequentialSizes(val) {
+function chainToSwitch(val) {
 	var answer = "";
-		switch(val) {
-			case 1:
-			case 2:
-			case 3:
-				answer = "Low";
-				break;
-			case 4:
-			case 5:
-			case 6:
-				answer = "Mid";
-				break;
-			case 7:
-			case 8:
-			case 9:
-				answer = "High";
-				break;			
+	switch(val) {
+		case "Bob":
+			answer = "Marley";
+			break;
+		case 42:
+			answer = "The Answer"
+			break;
+		case 1:
+			answer = "There is no #1";
+			break;
+		case 99:
+			answer = "Missed me by this much!";
+			break;
+		case 7:
+			answer = "Ate Nine";
+			break;
+	}
 	
-		}
 	return answer;
 }
 
-alert(sequentialSizes(9));
+alert(chainToSwitch(99));
