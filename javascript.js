9e9de94e746f9fae3b4817dfad1313a22f74a502
Igor Semenuk <teacherigor6@gmail.com>
2017-12-19 10:15:58 +0200

    multiple identical options in switch statements

--- javascript.js
+++ javascript.js
@@ -1,22 +1,25 @@
 'use strict';
-function switchOfStuff(val) {
+function sequentialSizes(val) {
 	var answer = "";
-	switch (val) {
-		case "a":
-			answer = "apple";
-			break;
-		case "b":
-			answer = "bird";
-			break;
-		case "c":
-			answer = "cat";
-			break;
-		default:
-			answer = "stuff";
-			break;
-	}
+		switch(val) {
+			case 1:
+			case 2:
+			case 3:
+				answer = "Low";
+				break;
+			case 4:
+			case 5:
+			case 6:
+				answer = "Mid";
+				break;
+			case 7:
+			case 8:
+			case 9:
+				answer = "High";
+				break;			
 	
+		}
 	return answer;
 }
 
-alert (switchOfStuff(""));
+alert(sequentialSizes(9));
