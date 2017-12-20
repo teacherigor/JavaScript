4e494acdbf72c76e654ee1b64247160d5da2ccb1
Igor Semenuk <teacherigor6@gmail.com>
2017-12-20 14:00:23 +0200

    if (a < 0 || b < 0)

--- javascript.js
+++ javascript.js
@@ -1,25 +1,22 @@
 'use strict';
-function chainToSwitch(val) {
-	var answer = "";
-	switch(val) {
-		case "Bob":
-			answer = "Marley";
-			break;
-		case 42:
-			answer = "The Answer"
-			break;
-		case 1:
-			answer = "There is no #1";
-			break;
-		case 99:
-			answer = "Missed me by this much!";
-			break;
-		case 7:
-			answer = "Ate Nine";
-			break;
+
+function abTest() {
+	/*
+	if (a < 0) {
+		return undefined;
+	} else if (b < 0) {
+		return undefined;
+	} else {
+		return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2)); 
+	}
+	*/
+	
+	if (a < 0 || b < 0) {
+		return undefined;
+	} else {
+		return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 	}
 	
-	return answer;
 }
 
-alert(chainToSwitch(99));
+alert(abTest(2,2));
