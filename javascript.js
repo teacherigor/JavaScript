c836f97ef7a46a5fe6fd14306a4825de961ff1fb
Igor Semenuk <teacherigor6@gmail.com>
2017-12-22 11:17:37 +0200

    dot operator

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var myDog = {
-	"name": "Chuck",
-	"legs": 4,
-	"tails": 1,
-	"friends": ["Everybody", "A ball"]
+var testObj = {
+	"hat": "ballcap",
+	"shirt": "jersey",
+	"shoes": "cleats"
 };
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
