35f77fad341ea2946feea2e228c88fc1e7ba4dd3
Igor Semenuk <teacherigor6@gmail.com>
2017-12-25 14:16:07 +0200

    myObj.hasOwnProperty(checkProp)

--- javascript.js
+++ javascript.js
@@ -1,11 +1,19 @@
 'use strict';
 
-function phoneticLookup(val) {
-	var result = "";
-	
-	var lookup = {
-		"alpha": "Adams",
-		"bravo": "Boston",
-		"charlie": "Chicago",
-	};
-}
+ var myObj = {
+	 gift: "pony",
+	 pet: "kitten",
+	 bed: "sleigh"
+ };
+ 
+ function checkObj(checkProp) {
+	 if(myObj.hasOwnProperty(checkProp)) {
+		 return myObj[checkProp];
+	 } else {
+		 return "Not Found";
+	 }
+	 
+	 return "Change Me!";
+ }
+ 
+ checkObj("gift");
