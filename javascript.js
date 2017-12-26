3fa9ff70db4ae34ede3c629aebe9e0c0d0aacc8d
Igor Semenuk <teacherigor6@gmail.com>
2017-12-26 16:13:12 +0200

    secondTree = myPlants[1].list[1];

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
 'use strict';
 
-var myStorage = {
-	"car": {
-		"inside": {
-			"glove box": "maps",
-			"passenger seat": "crumbs"
-		},
-		"outside": {
-			"trunk": "jack"
-		}
+var myPlants = [
+	{
+		type: "flowers",
+		list: [
+			"rose",
+			"tulip",
+			"dandelion"
+		]
+	},
+	{
+		type: "trees",
+		list: [
+			"fir",
+			"pine",
+			"birch"
+		]
 	}
-};
+];
 
-var gloveBoxContents = myStorage.car.inside["glove box];
+var secondTree = myPlants[1].list[1];
