c96ada21314be813489a18569f0394fa8183ca9b
Igor Semenuk <teacherigor6@gmail.com>
2017-12-26 14:52:39 +0200

    myStorage.car.inside[];

--- javascript.js
+++ javascript.js
@@ -1,22 +1,15 @@
 'use strict';
 
-var myMusic = [
-	{
-		"artist": "Billy Joel",
-		"title": "Piano Man",
-		"release_year": 1973,
-		"formats": [
-			"CS",
-			"BT",
-			"LP"],
-		"gold": true
-	},
-	{
-		"artist": "Billy Joel",
-		"title": "Streetlife Serenade",
-		"release_year": 1974,
-		"formats": [
-			"CS",
-			"LP"]
+var myStorage = {
+	"car": {
+		"inside": {
+			"glove box": "maps",
+			"passenger seat": "crumbs"
+		},
+		"outside": {
+			"trunk": "jack"
+		}
 	}
-];
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box];
