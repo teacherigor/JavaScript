ff64852d9e66159a0c7ac91c492747fb02d3ff91
Igor Semenuk <teacherigor6@gmail.com>
2017-12-28 12:39:33 +0200

    Nesting for loops

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
 'use strict';
-//Iterate through an array with a for loop
-var myArray = [2, 3, 4, 5, 6];
-var total = 0;
-
-for (var i = 0; i < myArray.length; i++) {
-	total += myArray[i];
-	console.log(total[i]);
+//Nesting for loops
+function multiplyAll(arr) {
+	var product = 1;
+	
+	for(var i = 0; i < arr.length; i++) {
+		for(var j = 0; j < arr[i].length; j++) {
+			product *= arr[i][j];
+		}
+	}
 }
+
+multiplyAll([[1,2], [3,4], [5,6,7]]);
