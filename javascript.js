3c2d0b5946072647254b28e532968d6bb5359478
Igor Semenuk <teacherigor6@gmail.com>
2017-12-28 16:53:22 +0200

    Profile lookout

--- javascript.js
+++ javascript.js
@@ -1,8 +1,32 @@
 'use strict';
-//Iterate with JS while loops
-var myArray = [];
-var i = 0;
-while (i < 5) {
-	myArray.push(i);
-	i++;
-}
+//Profile lookup
+var contacts = [
+	{
+		"firstName": "Akira",
+		"lastName": "Latine",
+		"number": "0543236543",
+		"likes": ["Pizza", "Coding", "Brownie Points"]
+	}
+	{
+		"firstName": "Harry",
+		"lastName": "Potter",
+		"number": "0994372684",
+		"likes": ["Hogwards", "Magic", "Hagrid"]
+	}
+	{
+		"firstName": "Sherlock",
+		"lastName": "Holmes",
+		"number": "0487345643",
+		"likes": ["Intriguing Cases", "Violin"]
+	}
+	{
+		"firstName": "Kristian",
+		"lastName": "Vos",
+		"number": "unknown",
+		"likes": ["JavaScript", "Gaming", "Foxes"]
+	}
+];
+
+function lookUpProfile(firstName, prop) {}
+
+lookUpProfile("Akira", "likes");
