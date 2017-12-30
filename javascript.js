200a031a090e897ef493c3fd43057553ddd67c7b
teacherigor <teacherigor6@gmail.com>
2017-12-30 16:53:51 +0200

    Math.floor(Math.random() * 10);

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 'use strict';
 
-var myArray = [];
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
 
-for (var i = 1; i < 6; i++) {
-	myArray.push(i);
+function randomWholeNum() {
+
+return Math.floor(Math.random() * 10);
+
 }
+
+alert(return);
